17be88a55c13c20a3ed1cd9d2a9a868223e0c9e7
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-04 17:59:02 +0000

    Add vote results page, url and template

--- vote_app/urls.py
+++ vote_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import VotingPage
+from .views import ResultsPage, VotingPage
 
 app_name = "vote_app"
 urlpatterns = [
-    path("", VotingPage.as_view(), name="vote")
+    path("new-vote/", VotingPage.as_view(), name="vote"),
+    path("results/", ResultsPage.as_view(), name="results"),
 ]
--- vote_app/views.py
+++ vote_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import Category, Candidate
 
 
 class VotingPage(TemplateView):
     """view for the voting page"""
     template_name = "vote/vote-page.html"
+
+    def get(self, request):
+        context = {
+            "categories": Category.objects.all(),
+        }
+        return render(request, self.template_name, context)
+
+
+class ResultsPage(TemplateView):
+    """view for the results"""
+    template_name = "vote/results-page.html"
+    
