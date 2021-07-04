18e5c703cd79dd526b235bb62685ac955915a0b9
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-04 01:03:19 +0000

    Register the user model on the admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.conf import settings
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username"]
+
+
+admin.site.register(User, UserAdmin)
