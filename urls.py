26ad0b8eb1b3f1e11fb4689f7673a18b42da7307
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-04 02:40:13 +0000

    Added homepage view and template and url conf

--- urls.py
+++ urls.py
@@ -7,6 +7,8 @@ from django.urls import path, include
 urlpatterns = [
     # add your own patterns here
     path('grappelli/', include('grappelli.urls')), # grappelli URLS
+
+    path("", include("pages.urls", namespace="pages")),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
