ffd19fd17fcfc37ca0fc3afc6e959f595b6c6dd3
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-04 15:48:07 +0000

    Register the category and candidate models on the admin interface

--- vote_app/admin.py
+++ vote_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Candidate, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ["full_name", "category", "number_of_votes"]
