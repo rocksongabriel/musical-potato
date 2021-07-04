16481396294a8214e9013a945d825e0982373e51
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-04 16:36:57 +0000

    Modify the admin display of the User model

--- users/models.py
+++ users/models.py
@@ -9,3 +9,7 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+# TODO - use the student id to log the students in
+# TODO - generate random password to each student that will be emailed to them
