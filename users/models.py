42bc05c11131ed1ea566862e4f0e7023bc16c5b3
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-04 16:32:41 +0000

    Add voted field to the user model

--- users/models.py
+++ users/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    """Model to represent a user"""
     
+    voted = models.BooleanField(default=False)
+
     def __str__(self):
         return self.username
