79dadca8f255ebf21a5d90259f320b0ee27d5b20
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-10 16:46:07 +0000

    Add login view and urls conf

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import UserLoginView
+
+
+app_name="users"
+urlpatterns = [
+    path("login/", UserLoginView.as_view(), name="login"),
+    # path("signup/", SignupView.as_view(), name="signup")
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls.base import reverse_lazy
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+from .forms import SignupForm
+
+
+# Login form
+class UserLoginView(LoginView):
+    template_name = "users/login.html"
+    redirect_field_name = reverse_lazy("vote_app:vote-categories")
+
+
+
+# class SignupView(TemplateView):
+#     """View to create a user account"""
+#     template_name = "users/signup.html"
+#     model = get_user_model()
+#     form_class = SignupForm
+#     success_url = reverse_lazy("vote_app:vote-categories")
+
+#     def get(self, request, **kwargs):
+#         context = {
+#             "form": self.form_class()
+#         }
+#         return render(request, self.template_name, context)
+
+#     def post(self, request, **kwargs):
+#         form = self.form_class(request.POST)
+#         context = {
+#             "form": self.form_class(initial=request.POST)
+#         }
+#         if form.is_valid():
+#             # create user object
+#             user = self.model().create_user(
+#                 username=request.POST['username'],
+#                 password=request.POST['password']
+#             )
+#             print(user)
+#             # authenticate the user 
+#             # log the user in 
+#             # redirect to the voting page
+
+#             print(request.POST)
+#             return redirect(self.success_url)
+#         return render(request, self.template_name, context)
