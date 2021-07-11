9f7724766bba750fa830bcc8267b125ab9b3b873
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-11 15:14:52 +0000

    Add design to the login form and page

--- static/js/custom/main.js
+++ static/js/custom/main.js
@@ -1,6 +1,7 @@
 let toggle_btn = document.getElementById("nav-toggle");
 let mobile_nav = document.getElementById("mobile-nav");
 
-toggle_btn.addEventListener("click", (event) => {
-    mobile_nav.classList.toggle("hidden")
+toggle_btn.addEventListener("click", () => {
+    mobile_nav.classList.toggle("flex");
+    mobile_nav.classList.toggle("hidden");
 })
