578df32d939c34888119faa7541ea5e00b7ebd7d
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-11 17:19:23 +0000

    Refactor: Change the names of url routes

--- vote_app/urls.py
+++ vote_app/urls.py
@@ -4,7 +4,7 @@ from .views import ResultsPage, VotingCategoriesListPage, VotingPage
 
 app_name = "vote_app"
 urlpatterns = [
-    path("voting-list/", VotingCategoriesListPage.as_view(), name="vote-categories"),
-    path("category/<slug:slug>/add-new-vote/", VotingPage.as_view(), name="vote"),
-    path("results/", ResultsPage.as_view(), name="results"),
+    path("election-categories/", VotingCategoriesListPage.as_view(), name="vote-categories"),
+    path("election-categories/<slug:slug>/add-new-vote/", VotingPage.as_view(), name="vote"),
+    path("election-results/", ResultsPage.as_view(), name="results"),
 ]
