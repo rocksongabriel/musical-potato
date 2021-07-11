2324a66bb2116a8ee30db95993eeaeba1bd5479b
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-11 18:09:14 +0000

    Add result pages urls

--- vote_app/urls.py
+++ vote_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import ResultsPage, VotingCategoriesListPage, VotingPage
+from .views import (AllCategoriesResultsPageView, ResultPageView,
+                    VotingCategoriesListPage, VotingPage)
 
 app_name = "vote_app"
 urlpatterns = [
     path("election-categories/", VotingCategoriesListPage.as_view(), name="vote-categories"),
     path("election-categories/<slug:slug>/add-new-vote/", VotingPage.as_view(), name="vote"),
-    path("election-results/", ResultsPage.as_view(), name="results"),
+
+    path("election-results/", AllCategoriesResultsPageView.as_view(), name="results"),
+    path("election-results/category/<slug:slug>/", ResultPageView.as_view(), name="result"),
 ]
