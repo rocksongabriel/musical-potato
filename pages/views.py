694491e35f6ac3ca00a4ee34b000c0a598fd50f0
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-14 23:10:55 +0000

    Add the Support form to the homepage

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from vote_app.forms import SupportForm
 
 
 class HomePage(TemplateView):
     template_name = "pages/homepage.html"
+    form_class = SupportForm
+    
+
+    def get(self, request, **kwargs):
+        context = {
+            "form": self.form_class()
+        }
+        return render(request, self.template_name, context)
