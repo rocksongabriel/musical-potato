c634a4687134cfc71946fddb35532588139b791a
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-15 12:42:54 +0000

    Use fontawesome cdn and minimized tailwindcss file

--- jstoolchain/tailwind.config.js
+++ jstoolchain/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: [
+    '../templates/pages/*.html',
+    '../templates/users/*.html',
+    '../templates/vote/*.html',
+    '../templates/*.html',
+    '../static/js/custom/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
