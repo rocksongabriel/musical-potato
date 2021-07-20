587052f12fc38aec72f5bda1d666cd1991806a37
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-20 02:05:49 +0000

    Add model to let the admin upload csv files to create the users and send the mails

--- users/models.py
+++ users/models.py
@@ -3,23 +3,18 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django.core.mail import send_mail
 import csv
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 
 # Custom User Manager
 class CustomUserManager(UserManager):
     """Custom user manager for the user model"""
-
     def auto_create_users(self, csv_file):
         """Create users from a csv file, using the student ID column as the the username, auto generate password for the account and email the credentials to the email associated with the student ID"""
 
-        # read the data from the csv file
-        # for each data in the csv, read the first item as the Student ID and the second as the email
-        # Create a user account using the Student ID as the username
-        # Email the Student ID and the Password to the email
-
         if csv_file is None:
-            raise ValueError("A csv file is required")  
-        
+            raise ValueError("A csv file is required")
+
         # open the csv file
         with open(csv_file, "r") as file:
             my_data_reader = csv.reader(file, delimiter=",")
@@ -33,18 +28,17 @@ class CustomUserManager(UserManager):
                 self.create_user(student_id, email, password)
 
                 message = f"Student ID: {student_id} -- Password: {password}"
-                send_mail(
-                    "Credentials for voting",
-                    message,
-                    from_email=settings.DEFAULT_FROM_EMAIL,
-                    recipient_list=[email],
-                    fail_silently=False
-                )
+                send_mail("Credentials for voting",
+                          message,
+                          from_email=settings.DEFAULT_FROM_EMAIL,
+                          recipient_list=[email],
+                          fail_silently=False)
                 print("----------------- Credentials Sent ----------------")
+
 
 class User(AbstractUser):
     """Model to represent a user"""
-    
+
     voted = models.BooleanField(default=False)
 
     objects = CustomUserManager()
@@ -53,5 +47,23 @@ class User(AbstractUser):
         return self.username
 
 
-# TODO - use the student id to log the students in
-# TODO - generate random password to each student that will be emailed to them
+class CreateAccountAndMailStudent(models.Model):
+    batch = models.CharField(
+        _("Name of Batch"),
+        max_length=100,
+        help_text="Enter name of batch you are uploading. eg. First Batch",
+        null=False,
+        blank=False)
+    csv = models.FileField(
+        _("CSV File"),
+        help_text="Upload the CSV file to create the accounts",
+        upload_to="accounts/csv/",
+        null=False,
+        blank=False)
+
+    def __str__(self):
+        return self.batch
+        
+    class Meta:
+        verbose_name = "Create Account and Mail Student"
+        verbose_name = "Create Account and Mail Student"
